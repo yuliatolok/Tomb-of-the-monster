0849218090f409f9b54366c569aa9b4058341bd4
Julia T <julchik.02@gmail.com>
2022-04-28 13:10:04 +0300

    Added arrow damage

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] GameObject destroyed;
     bool isFlying;
     void Update()
     {
         if (isFlying)
         {
-            transform.Translate(Vector3.up * Time.deltaTime, Space.Self);
+            transform.Translate(Vector3.up * Time.deltaTime * moveSpeed, Space.Self);
         }
     }
 
@@ -17,16 +19,30 @@ public class Arrow : MonoBehaviour
     {
         isFlying = false;
         gameObject.SetActive(false);
+        destroyed.SetActive(true);
+        destroyed.transform.position = transform.position;
 
+        Invoke("disableDestroyed", 2f);
     }
     public void Shoot()
     {
+        
         isFlying = true;
         gameObject.SetActive(true);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.TryGetComponent<Health>(out var health) && collision.CompareTag("Player"))
+        {
+            health.Kill();
+        }
         Explode();
+
+    }
+    private void disableDestroyed()
+    {
+        destroyed.SetActive(false);
+
     }
 
 }
